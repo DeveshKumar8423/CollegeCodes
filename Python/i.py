309978864a2dacbea0ea8eed312c762005bf7191
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-02-22 17:54:18 +0530

    program to addition of two complex numbers

---
+++ Python/i.py
@@ -0,0 +1,4 @@
+#program to additon of two complex numbers
+num1 = complex(input("Input first number : "))
+num2 = complex(input("Input second number : "))
+print("Addition of two numbers = " , num1 + num2)
