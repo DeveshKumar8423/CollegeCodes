eff1e190a3909553948e3d7405f4f157c6fbbeec
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-03-07 22:33:29 +0530

    chain comparison program

---
+++ Python/r.py
@@ -0,0 +1,6 @@
+#chain comparision program
+marks = int(input("Input Marks : "))
+if (1 < marks < 100) :
+    print("Marks is in between 1 and 100")
+else:
+    print("Marks is not between 1 and 100")
