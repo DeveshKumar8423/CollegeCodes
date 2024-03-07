d1f5088f4f4fc3fceee678ff9cc0c79a51ad416b
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-03-07 22:33:11 +0530

    Create q.py

---
+++ Python/q.py
@@ -0,0 +1,19 @@
+a , b = 11 , 5
+
+#Bitwise AND
+print(a & b)
+
+#Bitwise OR
+print(a | b)
+
+#Bitwise NOT
+print(~a)
+
+#Bitwise XOR
+print(a ^ b)
+
+#Bitwise right shift
+print(a << b)
+
+#Bitwise left shift
+print(a >> b)
