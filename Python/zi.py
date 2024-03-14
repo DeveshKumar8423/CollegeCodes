31f3579a1cf335f03ec53be31cf6f6a17c7561c5
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-03-14 22:03:55 +0530

    Create zi.py

---
+++ Python/zi.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    adding = lambda a,b : a+b
+    print("Sum of numbers are ", adding(a,b))
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+add(num1,num2)
