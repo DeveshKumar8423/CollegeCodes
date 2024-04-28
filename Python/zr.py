3943bdcd1d9908257223f02bea9f70a7e6deefd3
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-04-29 00:00:29 +0530

    Create zr.py

---
+++ Python/zr.py
@@ -0,0 +1,11 @@
+data = [(1,2),(2,3),(3,4),(4,5),(5,6)]
+x_coordinates = []
+y_coordinates = []
+
+for i in data:
+    x = i[0]
+    y = i[1]
+    x_coordinates.append((x))
+    y_coordinates.append((x))
+
+print(x_coordinates,y_coordinates)
