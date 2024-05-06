d035b29ae84489f57dfd26162f5056c1e6f7646b
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-05-06 23:41:21 +0530

    Memory allocation functions

--- DSA/g.c
+++ DSA/g.c
@@ -1,1 +1,26 @@
 //Memory allocation functions
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr;
+
+    ptr = (int*)malloc(5 * sizeof(int));
+    
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        ptr[i] = i + 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("Value at index %d: %d\n", i, ptr[i]);
+    }
+
+    free(ptr);
+
+    return 0;
+}
