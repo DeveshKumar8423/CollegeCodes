61d069a74170a0887bf90dcf0e4657ce8c20d227
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-05-12 11:40:53 +0530

    Update e.c

--- DSA/e.c
+++ DSA/e.c
@@ -1,2 +1,24 @@
 //Maximum and Minimum of a given array
+#include <stdio.h>
 
+int main()
+{
+    int arr[] = {10, 5, 20, 8, 15};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++){
+        if (arr[i] < min){
+            min = arr[i];
+        }
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    printf("Minimum element in array: %d\n", min);
+    printf("Maximum element in array: %d\n", max);
+
+    return 0;
+}
